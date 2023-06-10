f8c5833aaae1dda79b086ab96a3f03fbfb454d4f
zulvaaraf <zulvasebelas@gmail.com>
2023-06-11 00:40:02 +0700

    fix error get albums id

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -29,7 +29,7 @@ class AlbumsHandler {
   async getAlbumByIdHandler(request) {
     this._validator.validateAlbumPayload(request.payload);
     const { id } = request.params;
-    const album = this._service.getAlbumById(id);
+    const album = await this._service.getAlbumById(id);
     return {
       status: 'success',
       data: {
