f40a69c31051c5cb3b133f762a1d879159ae7685
zulvaaraf <zulvasebelas@gmail.com>
2023-06-11 11:51:46 +0700

    mapping

--- src/utils/indexAlbums.js
+++ src/utils/indexAlbums.js
@@ -1,7 +1,9 @@
-const mapDBToModelAlbums = ({ id, name, year }) => ({
+const mapDBToModelAlbums = ({ id, name, year, created_at, updated_at }) => ({
   id,
   name,
   year,
+  created_at: created_at,
+  updated_at: updated_at,
 });
 
 module.exports = { mapDBToModelAlbums };
