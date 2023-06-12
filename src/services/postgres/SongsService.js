386e83beb78b15bb5b6c2f31320a0dd05ee68db3
zulvaaraf <zulvasebelas@gmail.com>
2023-06-12 21:24:42 +0700

    get song by id implemented

---
+++ src/services/postgres/SongsService.js
@@ -0,0 +1,68 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const { mapDBTtoModelSongs } = require('../../utils/indexSongs');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class SongsValidator {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO songs VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongs(title, performer) {
+    let result = await this._pool.query('SELECT id, title, performer FROM songs');
+
+    if (title !== undefined) {
+      const query = {
+        text: 'SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE $1',
+        values: [`%${title}%`],
+      };
+      result = await this._pool.query(query);
+    }
+
+    if (performer !== undefined) {
+      const query = {
+        text: 'SELECT id, title, performer FROM songs WHERE LOWER(title) LIKE $1',
+        values: [`%${title}%`],
+      };
+
+      result = await this._pool.query(query);
+    }
+
+    return result.rows.map(mapDBTtoModelSongs);
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBTtoModelSongs)[0];
+  }
+}
+
+module.exports = SongsValidator;
