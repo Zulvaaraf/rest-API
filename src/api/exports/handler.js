cbd20480883528e89ccf8001422ac0599dfed953
zulvaaraf <zulvasebelas@gmail.com>
2023-07-14 00:16:35 +0700

    export handler/plugin implemented

---
+++ src/api/exports/handler.js
@@ -0,0 +1,36 @@
+const autoBind = require('auto-bind');
+
+class ExportsHandler {
+  constructor(exportsService, playlistsService, validator) {
+    this._exportsService = exportsService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postExportSongsHandler(request, h) {
+    this._validator.validateExportSongsPayload(request.payload);
+
+    const { playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+
+    const message = {
+      userId: request.auth.credentials.id,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._exportsService.sendMessage('export:playlists', JSON.stringify(message));
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan anda dalam antrean',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
