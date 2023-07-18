e0492b1c8357f4fdc8607d0398ca64b1f8a66336
zulvaaraf <zulvasebelas@gmail.com>
2023-07-18 13:41:53 +0700

    adding column coverUrl

--- src/utils/indexAlbums.js
+++ src/utils/indexAlbums.js
@@ -1,7 +1,8 @@
-const mapDBToModelAlbums = ({ id, name, year, created_at, updated_at }) => ({
+const mapDBToModelAlbums = ({ id, name, year, coverUrl, created_at, updated_at }) => ({
   id,
   name,
   year,
+  coverUrl,
   created_at: created_at,
   updated_at: updated_at,
 });
