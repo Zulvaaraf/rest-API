c5c9a93922cbfba49fc78d3ea329c355f82ba343
zulvaaraf <zulvasebelas@gmail.com>
2023-07-23 13:26:04 +0700

    solving query get

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -29,7 +29,7 @@ class AlbumsService {
 
   async getAlbumById(id) {
     const query = {
-      text: 'SELECT id, name, year, "coverUrl" FROM albums WHERE id = $1',
+      text: 'SELECT * FROM albums WHERE id = $1',
       values: [id],
     };
 
